e871e24d46799e2d91444b1df3809fb00349ba71
pedrh2s <47404690+pedroh2s@users.noreply.github.com>
2020-03-13 20:33:19 -0300

    metodos adicionados no userController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -26,5 +26,48 @@ module.exports = {
         const users = await User.find();
 
         return res.json(users);
-    }
+    },
+
+    async show(req, res){
+        const {id: _id} = req.params;
+
+        const user = await User.findOne({_id});
+
+        if(!user){
+            return res.status(400).json({message: "The idUser doesn't exists"});
+        }
+
+        return res.json(user);
+    },
+
+    async update (req, res){
+        const {id} = req.params;
+
+        const {first_name, last_name, user_name, password} = req.body;
+
+        const password_hash = password + '123';
+
+        /* const userExist = await User.findOne({ user_name });
+
+        if(userExist){
+            return res.status(400).json({message: "Username already exists"});
+        } */
+
+        const user = await User.findByIdAndUpdate(id,{
+            first_name,
+            last_name,
+            user_name,
+            password_hash,
+        });
+
+        return res.json(user);
+    },
+    
+    async destroy (req, res){
+        const {id} = req.params;
+
+        await User.findByIdAndRemove(id);
+
+        return res.json({message: 'delete success!!!'});
+    }    
 }
