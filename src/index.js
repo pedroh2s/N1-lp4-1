c6deac970934827a91972ee22fc99be7d2490a2a
Caio favoretto <caiowfavoretto@gmail.com>
2020-03-16 23:23:54 -0300

    feature: cors added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
-mongoose.connect('mongodb+srv://n1_ftt_job:n1_ftt_job@cluster0-wejnp.mongodb.net/n113?retryWrites=true&w=majority',{
+mongoose.connect(
+  'mongodb+srv://n1_ftt_job:n1_ftt_job@cluster0-wejnp.mongodb.net/n113?retryWrites=true&w=majority',
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+    useUnifiedTopology: true
+  }
+);
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
-
